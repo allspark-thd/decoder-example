e73e3de031666adf32df7ad63e4f168415aada1a
gambtho <thomas_gamble@homedepot.com>
2016-01-31 12:57:18 -0500

    added sql tasks to support integration test

---
+++ src/test/java/com/homedepot/integration/Full_Integration.java
@@ -0,0 +1,15 @@
+package com.homedepot.integration;
+
+import org.junit.Test;
+
+/*  Assumes maria DB is installed, with user dummy@localhost.com
+ *  and password 'dummy_pass'
+ *
+ */
+
+public class Full_Integration {
+    @Test
+    public void testConnect() throws Exception {
+
+    }
+}
